dd826343e63412591bb333b0fbe962be0fc651d7
David Uhlmann <dib542@gmail.com>
2023-04-15 03:34:22 +1000

    feat: add block catch up handling

---
+++ src/storage/sqlite3/ingest/block.mjs
@@ -0,0 +1,37 @@
+
+import db from '../db.mjs';
+
+let insertTx;
+async function insertBlockRows(block) {
+  // activate at run time (after db has been initialized)
+  insertTx = insertTx || db.prepare(`
+    INSERT INTO 'block' (
+      'block_id.hash',
+      'header.version.block',
+      'header.height',
+      'header.time',
+      'header.time_unix'
+    ) values (?, ?, ?, ?, ?)
+  `);
+
+  return new Promise((resolve, reject) => {
+    insertTx.run([
+      // 'block_id.hash' TEXT NOT NULL,
+      block.block_id.hash,
+      // 'header.version.block' INTEGER NOT NULL,
+      block.block.header.version.block,
+      // 'header.height' INTEGER PRIMARY KEY NOT NULL,
+      block.block.header.height,
+      // 'header.time' TEXT NOT NULL,
+      block.block.header.time,
+      // 'header.time_unix' INTEGER UNIQUE NOT NULL
+      Math.round(new Date(block.block.header.time).valueOf() / 1000),
+    ], err => err ? reject(err) : resolve());
+  });
+}
+
+export default async function ingestBlocks (blockPage) {
+  return await Promise.all(blockPage.map(async block => {
+    await insertBlockRows(block);
+  }));
+};
