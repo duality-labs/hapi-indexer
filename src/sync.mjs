2bdd5d83cae57150e5e50d510ff1a9c2e49995cc
David Uhlmann <dib542@gmail.com>
2023-04-15 01:37:22 +1000

    refactor: abstract out fetch page iteration logic

--- src/sync.mjs
+++ src/sync.mjs
@@ -4,36 +4,38 @@ const { RPC_API='' } = process.env;
 
 async function iterateThroughPages(readPage) {
 
-  let currentPage = 0;
+  let currentPage;
   let currentItemCount = 0;
   let previousItemCount = 0;
-  let totalItemCount = 0;
   do {
-    const [pageItemCount, totalItemCount, nextPage = currentPage + 1] = await readPage({ page: currentPage });
+    const [pageItemCount, totalItemCount, nextPage] = await readPage({ page: currentPage });
 
     // update progress
     previousItemCount = currentItemCount;
     currentItemCount += pageItemCount;
-    totalItemCount = totalItemCount;
     currentPage = nextPage;
 
     // see progress
     console.log(` import progress: ${Math.round(100*currentItemCount/totalItemCount).toFixed(0).padStart(3, ' ')}%`);
-  } while (currentItemCount > previousItemCount && currentItemCount < totalItemCount);
+  } while (currentItemCount > previousItemCount && !!currentPage);
 
 };
 
 export async function catchUp () {
 
   // read transaction pages
-  await iterateThroughPages(async ({ page }) => {
-    const response = await fetch(`${RPC_API}/tx_search?query="tx.height>=0"&page=${page}&per_page=100`);
+  await iterateThroughPages(async ({ page = 1 }) => {
+    // we default starting page to 1 as this API has 1-based page numbers
+    const itemsPerPage = 100;
+    const response = await fetch(`${RPC_API}/tx_search?query="tx.height>=0"&page=${page}&per_page=${itemsPerPage}`);
     const { result } = await response.json();
 
     // ingest all tx rows given
     await ingestTxs(result.txs);
-    // return information for page iterating function
-    return [result.txs.length, result.totalCount, page + 1];
+    // return next page information for page iterating function
+    const totalItemCount = (page - 1) * itemsPerPage + result.txs.length;
+    const nextPage = totalItemCount < result.totalCount ? page + 1 : null;
+    return [result.txs.length, result.totalCount, nextPage];
   });
 
 };
