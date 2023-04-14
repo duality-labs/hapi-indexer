7622f42e9e20d6dbf8c0202e7613f53b41b44cfa
David Uhlmann <dib542@gmail.com>
2023-04-15 01:20:18 +1000

    refactor: abstract out fetch page iteration logic

--- src/sync.mjs
+++ src/sync.mjs
@@ -2,28 +2,39 @@ import ingestTxs from './storage/sqlite3/ingest/tx.mjs';
 
 const { RPC_API='' } = process.env;
 
+async function iterateThroughPages(readPage) {
+
+  let currentPage = 0;
+  let currentItemCount = 0;
+  let previousItemCount = 0;
+  let totalItemCount = 0;
+  do {
+    const [pageItemCount, totalItemCount, nextPage = currentPage + 1] = await readPage({ page: currentPage });
+
+    // update progress
+    previousItemCount = currentItemCount;
+    currentItemCount += pageItemCount;
+    totalItemCount = totalItemCount;
+    currentPage = nextPage;
+
+    // see progress
+    console.log(` import progress: ${Math.round(100*currentItemCount/totalItemCount).toFixed(0).padStart(3, ' ')}%`);
+  } while (currentItemCount > previousItemCount && currentItemCount < totalItemCount);
+
+};
+
 export async function catchUp () {
 
-  let currentTxPage = 0;
-  let currentTxCount = 0;
-  let previousTxCount = 0;
-  let totalTxCount = 0;
-  do {
-    currentTxPage += 1;
-    const response = await fetch(`${RPC_API}/tx_search?query="tx.height>=0"&page=${currentTxPage}&per_page=100`);
+  // read transaction pages
+  await iterateThroughPages(async ({ page }) => {
+    const response = await fetch(`${RPC_API}/tx_search?query="tx.height>=0"&page=${page}&per_page=100`);
     const { result } = await response.json();
 
     // ingest all tx rows given
     await ingestTxs(result.txs);
-
-    // update progress
-    previousTxCount = currentTxCount;
-    currentTxCount += result.txs.length;
-    totalTxCount = result.total_count;
-
-    // see progress
-    console.log(`tx import progress: ${Math.round(100*currentTxCount/totalTxCount).toFixed(0).padStart(3, ' ')}%`);
-  } while (currentTxCount > previousTxCount && currentTxCount < totalTxCount);
+    // return information for page iterating function
+    return [result.txs.length, result.totalCount, page + 1];
+  });
 
 };
 
