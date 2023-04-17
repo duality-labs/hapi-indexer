48772696a91d6b85038308ea96051b7134783b90
David Uhlmann <dib542@gmail.com>
2023-04-18 05:02:37 +1000

    feat: allow faster block catchup method:
        - the smaller block height scope allows for less of the db to be
          read into memory to answer this query, for large chains this
          can be a very significant saving (>30x difference
        - see issue: https://github.com/cometbft/cometbft/issues/517
        - the drawback is that the total blocks paginated is no longer
          known, we now estimate it using the chain height at start
          (import progress may go over 100% before finishing)

--- src/sync.mjs
+++ src/sync.mjs
@@ -37,22 +37,10 @@ const pollingLogger = createLogger({
 });
 
 async function iterateThroughPages(readPage, logger) {
-  let lastProgress;
   let lastProgressTime = 0;
-  function printProgress(fraction, forcePrint) {
-    const progress = (100 * fraction).toFixed(1);
-    if (forcePrint || (progress !== lastProgress && Date.now() - lastProgressTime > 1000)) {
-      // add special lines for start and end of import
-      if (fraction === 0) {
-        logger.info('import starting');
-      }
-      else if (fraction === 1) {
-        logger.info('import done');
-      }
-      else {
-        logger.info(`import progress: ${progress.padStart(3, ' ')}%`);
-      }
-      lastProgress = progress;
+  function printProgress(fraction, message) {
+    if (message || (Date.now() - lastProgressTime > 1000)) {
+      logger.info(message || `import progress: ${(100 * fraction).toFixed(1).padStart(3, ' ')}%`);
       lastProgressTime = Date.now();
     }
   }
@@ -61,7 +49,7 @@ async function iterateThroughPages(readPage, logger) {
   let currentItemCount = 0;
   let previousItemCount = 0;
 
-  printProgress(0, true);
+  printProgress(0, 'import starting');
   do {
     // read page data and return counting details
     const [pageItemCount, totalItemCount, nextPage] = await readPage({ page: currentPage });
@@ -74,20 +62,27 @@ async function iterateThroughPages(readPage, logger) {
     // see progress
     printProgress(currentItemCount / totalItemCount);
   } while (currentItemCount > previousItemCount && !!currentPage);
-  printProgress(1, true);
+  printProgress(1, 'import done');
 };
 
 let maxBlockHeight = 0;
 export async function catchUp ({ fromBlockHeight = 0, logger = defaultLogger }={}) {
+
+  const totalBlockCount = await fetch(`${RPC_API}/abci_info`)
+    .then(response => response.json())
+    .then(({ result }) => Number(result.response.last_block_height) - fromBlockHeight);
 
   // read block pages
   await iterateThroughPages(async ({ page = 1 }) => {
     // we default starting page to 1 as this API has 1-based page numbers
     // max API response page item count is 100
     const itemsPerPage = 100;
-    const response = await fetch(`${RPC_API}/block_search?query="block.height>${fromBlockHeight}"&page=${page}&per_page=${itemsPerPage}&order_by="asc"`);
+    const heightStart = (page - 1) * itemsPerPage + fromBlockHeight;
+    const heightEnd = page * itemsPerPage + fromBlockHeight;
+    const blockQuery = `block.height>${heightStart}+AND+block.height<=${heightEnd}`;
+    const response = await fetch(`${RPC_API}/block_search?query="${blockQuery}"&page=1&per_page=${itemsPerPage}&order_by="asc"`);
     const { result } = await response.json();
-    const { blocks: pageItems, total_count: totalItemCount } = result;
+    const { blocks: pageItems } = result;
 
     // take note of the maximum block height stored so far
     const lastBlockHeight = Number(pageItems[pageItems.length - 1]?.block?.header?.height);
@@ -100,8 +95,8 @@ export async function catchUp ({ fromBlockHeight = 0, logger = defaultLogger }={
 
     // return next page information for page iterating function
     const currentItemCount = (page - 1) * itemsPerPage + pageItems.length;
-    const nextPage = currentItemCount < totalItemCount ? page + 1 : null;
-    return [pageItems.length, totalItemCount, nextPage];
+    const nextPage = currentItemCount < totalBlockCount ? page + 1 : null;
+    return [pageItems.length, totalBlockCount, nextPage];
   }, logger.child({ label: 'block' }));
 
   // read transaction pages
