d17274e68c8065f2f32eda637e9493bc3093684d
David Uhlmann <dib542@gmail.com>
2023-04-18 13:57:40 +1000

    feat: add debug route to display all table data in dev environment

--- src/server.mjs
+++ src/server.mjs
@@ -4,6 +4,8 @@ import logger from './logger.mjs';
 import * as db from './storage/sqlite3/index.mjs';
 import * as sync from './sync.mjs';
 
+import dbClient from './storage/sqlite3/db.mjs'
+
 import { volume } from './storage/sqlite3/stats.mjs';
 
 const init = async () => {
@@ -22,6 +24,44 @@ const init = async () => {
     method: 'GET',
     path: '/',
     handler: () => 'ok',
+  });
+
+  if (process.env.NODE_ENV === 'development') server.route({
+    method: 'GET',
+    path: '/debug',
+    handler: async (_, h) => {
+      try {
+        const tableNames = await new Promise((resolve, reject) => {
+          dbClient.all(`SELECT name FROM sqlite_schema WHERE type='table' ORDER BY name`, [], (err, rows) => {
+            if (err) {
+              reject(err);
+            }
+            else {
+              resolve(rows.map(row => row.name))
+            }
+          });
+        })
+        return Promise.all(tableNames.filter(name => name !== 'sqlite_sequence').map(tableName => {
+          return new Promise((resolve, reject) => {
+            dbClient.all(`SELECT * FROM '${tableName}'`, [], (err, rows) => {
+              if (err) {
+                reject(err);
+              }
+              else {
+                resolve([tableName, rows]);
+              }
+            });
+          });
+        })).then(tables => tables.reduce((acc, [tableName, rows], index) => {
+          acc[tableName] = rows;
+          return acc;
+        }, {}));
+      }
+      catch (err) {
+        console.log('err', err);
+        return h.response(`something happened: ${err.message || '?'}`).code(500);
+      }
+    },
   });
 
   server.route({
