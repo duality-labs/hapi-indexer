bbd8f4ba12ec175bb4f5df53ddd226c275487db4
David Uhlmann <dib542@gmail.com>
2023-05-04 01:57:54 +1000

    refactor: set database file through environment vars

--- src/storage/sqlite3/db.mjs
+++ src/storage/sqlite3/db.mjs
@@ -1,12 +1,14 @@
 import sqlite3 from 'sqlite3'
 import { open } from 'sqlite'
 
-if (process.env.NODE_ENV === 'development') {
+const { NODE_ENV, DB_FILENAME='/tmp/database.db' } = process.env;
+
+if (NODE_ENV === 'development') {
   sqlite3.verbose();
 }
 
 export const dbPromise = open({
-  filename: ':memory:',
+  filename: DB_FILENAME,
   driver: sqlite3.Database
 });
 
