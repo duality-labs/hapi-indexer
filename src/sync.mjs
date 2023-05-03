0fd79085e12d1e4de13a9e33b5ff6269cf2c4949
David Uhlmann <dib542@gmail.com>
2023-05-03 13:13:42 +1000

    refactor: remove unused RPC methods

--- src/sync.mjs
+++ src/sync.mjs
@@ -1,11 +1,9 @@
 import { createLogger, transports, config, format } from 'winston';
 import { logFileTransport } from './logger.mjs';
 
-import ingestBlocks from './storage/sqlite3/ingest/rpc/block.mjs';
-import ingestRpcTxs from './storage/sqlite3/ingest/rpc/tx.mjs';
 import ingestRestTxs from './storage/sqlite3/ingest/rest/tx.mjs';
 
-const { RPC_API='', REST_API='', POLLING_INTERVAL_SECONDS='' } = process.env;
+const { REST_API='', POLLING_INTERVAL_SECONDS='' } = process.env;
 
 const pollIntervalTimeSeconds = Number(POLLING_INTERVAL_SECONDS) || 5;
 
@@ -67,85 +65,6 @@ async function iterateThroughPages(readPage, logger) {
 };
 
 let maxBlockHeight = 0;
-async function catchUpRPC ({ fromBlockHeight = 0, logger = defaultLogger }={}) {
-
-  const totalBlockCount = await fetch(`${RPC_API}/abci_info`)
-    .then(response => response.json())
-    .then(({ result }) => Number(result.response.last_block_height) - fromBlockHeight);
-
-  // read block pages
-  await iterateThroughPages(async ({ page = 1 }) => {
-    // we default starting page to 1 as this API has 1-based page numbers
-    // max API response page item count is 100
-    const itemsPerPage = 100;
-    const heightStart = (page - 1) * itemsPerPage + fromBlockHeight;
-    const heightEnd = page * itemsPerPage + fromBlockHeight;
-    const blockQuery = `block.height>${heightStart}+AND+block.height<=${heightEnd}`;
-    const response = await fetch(`${RPC_API}/block_search?query="${blockQuery}"&page=1&per_page=${itemsPerPage}&order_by="asc"`);
-    const { result } = await response.json();
-    const { blocks: pageItems } = result;
-
-    // take note of the maximum block height stored so far
-    const lastBlockHeight = Number(pageItems[pageItems.length - 1]?.block?.header?.height);
-    if (lastBlockHeight > maxBlockHeight) {
-      maxBlockHeight = lastBlockHeight;
-    }
-
-    // ingest list
-    await ingestBlocks(pageItems);
-
-    // return next page information for page iterating function
-    const currentItemCount = (page - 1) * itemsPerPage + pageItems.length;
-    const nextPage = currentItemCount < totalBlockCount ? page + 1 : null;
-    return [pageItems.length, totalBlockCount, nextPage];
-  }, logger.child({ label: 'block' }));
-
-  // read transaction pages
-  await iterateThroughPages(async ({ page = 1 }) => {
-    // we default starting page to 1 as this API has 1-based page numbers
-    const itemsPerPage = 100;
-    const response = await fetch(`${RPC_API}/tx_search?query="tx.height>${fromBlockHeight}"&page=${page}&per_page=${itemsPerPage}`);
-    const { result } = await response.json();
-    const { txs: pageItems, total_count: totalItemCount } = result;
-
-    // ingest list
-    await ingestRpcTxs(pageItems);
-
-    // return next page information for page iterating function
-    const currentItemCount = (page - 1) * itemsPerPage + pageItems.length;
-    const nextPage = currentItemCount < totalItemCount ? page + 1 : null;
-    return [pageItems.length, totalItemCount, nextPage];
-  }, logger.child({ label: 'transaction' }));
-
-};
-
-export async function keepUpRPC () {
-
-  defaultLogger.info(`keeping up: polling from block height: ${maxBlockHeight}`)
-
-  // poll for updates
-  async function poll() {
-
-    defaultLogger.info(`keeping up: polling`);
-    const lastBlockHeight = maxBlockHeight;
-    await catchUpRPC({ fromBlockHeight: maxBlockHeight, logger: pollingLogger });
-
-    // log block height increments
-    if (maxBlockHeight > lastBlockHeight) {
-      for (let i = lastBlockHeight + 1; i <= maxBlockHeight; i += 1) {
-        defaultLogger.info(`keeping up: new block processed: ${i}`);
-      }
-    }
-
-    // poll again after a certain amount of time has passed
-    // note: prefer setTimeout over setInterval because of concerns about
-    // overlapping ingestions into the DB (ie. if ingestion takes longer than
-    // the setInterval time then multiple invocations of catchUp will run concurrently)
-    setTimeout(poll, 1000 * pollIntervalTimeSeconds);
-  }
-
-  poll();
-};
 
 async function catchUpREST ({ fromBlockHeight = 0, logger = defaultLogger }={}) {
 
