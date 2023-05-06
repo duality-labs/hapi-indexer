3d1a4d9acda87ca59827ada16b3da450eff4ecff
David Uhlmann <dib542@gmail.com>
2023-05-07 00:16:25 +1000

    refactor: simplify awaiting in series logic with for loops

--- src/storage/sqlite3/ingest/ingestTxResponse.ts
+++ src/storage/sqlite3/ingest/ingestTxResponse.ts
@@ -12,65 +12,60 @@ import insertEventDeposit from './tables/event.Deposit';
 import insertEventWithdraw from './tables/event.Withdraw';
 import { upsertDerivedTickStateRows } from './tables/derived.tick_state';
 
-import decodeEvent, { DecodedTxEvent } from './utils/decodeEvent';
+import decodeEvent from './utils/decodeEvent';
 import { getDexMessageAction, isValidResult } from './utils/utils';
 
 export default async function ingestTxs(txPage: TxResponse[]) {
-  return await promiseMapInSeries(
-    txPage,
-    async (tx_result: TxResponse, index: number) => {
-      // skip invalid transactions
-      if (!isValidResult(tx_result)) {
-        return;
-      }
-
-      const txEvents = (tx_result.events || []).map(decodeEvent);
-      // first add block rows
-      await insertBlockRows(tx_result);
-      // then add token foreign keys
-      await promiseMapInSeries(txEvents, insertDexTokensRows);
-      // then add pair foreign keys
-      await promiseMapInSeries(txEvents, insertDexPairsRows);
-      // then add transaction rows
-      await insertTxRows(tx_result, index);
-      // then add transaction event rows
-      await promiseMapInSeries(txEvents, async (txEvent: DecodedTxEvent) => {
-        await insertTxEventRows(tx_result, txEvent, index);
-
-        // continue logic for dex events
-        // if the event was a dex action then use that event to update tables
-        const dexAction = getDexMessageAction(txEvent);
-        if (dexAction) {
-          // add event rows to specific event tables:
-          switch (dexAction) {
-            case 'Deposit':
-              await insertEventDeposit(tx_result, txEvent, index);
-              break;
-            case 'Withdraw':
-              await insertEventWithdraw(tx_result, txEvent, index);
-              break;
-            case 'Swap':
-              await insertEventSwap(tx_result, txEvent, index);
-              break;
-            case 'TickUpdate':
-              await insertEventTickUpdate(tx_result, txEvent, index);
-              await upsertDerivedTickStateRows(tx_result, txEvent, index);
-              break;
-          }
-        }
-      });
+  for (const [index, tx_result] of txPage.entries()) {
+    // skip invalid transactions
+    if (!isValidResult(tx_result)) {
+      return;
     }
-  );
-}
 
-async function promiseMapInSeries<T>(
-  list: Array<T>,
-  itemCallback: (item: T, index: number, list: T[]) => Promise<unknown>
-) {
-  return list.reduce<Promise<unknown[]>>(async (listPromise, item, index) => {
-    return Promise.all([
-      ...(await listPromise),
-      itemCallback(item, index, list),
-    ]);
-  }, new Promise((resolve) => resolve([])));
+    // get tx events in decoded form
+    const txEvents = (tx_result.events || []).map(decodeEvent);
+
+    // first add block rows
+    await insertBlockRows(tx_result);
+
+    // then add token foreign keys
+    for (const txEvent of txEvents) {
+      await insertDexTokensRows(txEvent);
+    }
+
+    // then add pair foreign keys
+    for (const txEvent of txEvents) {
+      await insertDexPairsRows(txEvent);
+    }
+
+    // then add transaction rows
+    await insertTxRows(tx_result, index);
+
+    // then add transaction event rows
+    for (const txEvent of txEvents) {
+      await insertTxEventRows(tx_result, txEvent, index);
+
+      // continue logic for dex events
+      // if the event was a dex action then use that event to update tables
+      const dexAction = getDexMessageAction(txEvent);
+      if (dexAction) {
+        // add event rows to specific event tables:
+        switch (dexAction) {
+          case 'Deposit':
+            await insertEventDeposit(tx_result, txEvent, index);
+            break;
+          case 'Withdraw':
+            await insertEventWithdraw(tx_result, txEvent, index);
+            break;
+          case 'Swap':
+            await insertEventSwap(tx_result, txEvent, index);
+            break;
+          case 'TickUpdate':
+            await insertEventTickUpdate(tx_result, txEvent, index);
+            await upsertDerivedTickStateRows(tx_result, txEvent, index);
+            break;
+        }
+      }
+    }
+  }
 }
