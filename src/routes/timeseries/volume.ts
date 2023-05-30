84ce9f33819359c5816a9079d34c4a5b19027f23
David Uhlmann <dib542@gmail.com>
2023-05-31 04:36:24 +1000

    feat: add accumulation of swap volume times

--- src/routes/timeseries/volume.ts
+++ src/routes/timeseries/volume.ts
@@ -3,17 +3,96 @@ import { Request, ResponseToolkit } from '@hapi/hapi';
 import logger from '../../logger';
 import getVolumePerSecond from '../../storage/sqlite3/db/derived.tx_price_data/getVolumePerSecond';
 
+interface DataRow {
+  timestamp: string;
+  tokenA: number;
+  tokenB: number;
+}
+type Data = Array<DataRow>;
+
+function accumulateData(data: Data, { timestamp, tokenA, tokenB }: DataRow) {
+  const dataLast = data[data.length - 1];
+  if (dataLast && dataLast.timestamp === timestamp) {
+    if (tokenA) {
+      dataLast.tokenA += tokenA;
+    }
+    if (tokenB) {
+      dataLast.tokenB += tokenB;
+    }
+  } else {
+    data.push({ timestamp, tokenA, tokenB });
+  }
+  return data;
+}
+
 const routes = [
   {
     method: 'GET',
     path: '/timeseries/volume/{tokenA}/{tokenB}',
     handler: async (request: Request, h: ResponseToolkit) => {
       try {
-        return await getVolumePerSecond(
+        const volumePerSecond = await getVolumePerSecond(
           request.params['tokenA'],
           request.params['tokenB'],
           request.query // the time extents and frequency and such
         );
+
+        const volumePerHour = volumePerSecond.data.reduceRight<Data>(
+          (data, [timeUnix, amount, token]) => {
+            const date = new Date(timeUnix * 1000);
+            date.setMilliseconds(0);
+            date.setSeconds(0);
+            date.setMinutes(0);
+            const row: DataRow = {
+              timestamp: date.toISOString(),
+              tokenA: token === request.params['tokenA'] ? amount : 0,
+              tokenB: token === request.params['tokenB'] ? amount : 0,
+            };
+            return accumulateData(data, row);
+          },
+          []
+        );
+
+        const volumePerDay = volumePerHour.reduceRight<Data>(
+          (data, { timestamp, tokenA, tokenB }) => {
+            const date = new Date(timestamp);
+            date.setHours(0);
+            return accumulateData(data, {
+              timestamp: date.toISOString(),
+              tokenA,
+              tokenB,
+            });
+          },
+          []
+        );
+
+        const volumePerWeek = volumePerDay.reduceRight<Data>(
+          (data, { timestamp, tokenA, tokenB }) => {
+            const date = new Date(timestamp);
+            const dayOfWeek = date.getDay();
+            // set day of month to first of a week by using dayOfWeek
+            date.setDate(date.getDate() - dayOfWeek);
+            return accumulateData(data, {
+              timestamp: date.toISOString(),
+              tokenA,
+              tokenB,
+            });
+          },
+          []
+        );
+
+        return {
+          '24H': { resolution: 'hour', data: volumePerHour.slice(0, 24) },
+          '1W': { resolution: 'hour', data: volumePerHour.slice(0, 24 * 7) },
+          '1M': { resolution: 'day', data: volumePerDay.slice(0, 28) },
+          '1Y': { resolution: 'week', data: volumePerWeek.slice(0, 52) },
+          ALL:
+            volumePerWeek.length > 52
+              ? { resolution: 'week', data: volumePerWeek }
+              : volumePerDay.length > 28
+              ? { resolution: 'day', data: volumePerDay }
+              : { resolution: 'hour', data: volumePerHour },
+        };
       } catch (err: unknown) {
         if (err instanceof Error) {
           logger.error(err);
