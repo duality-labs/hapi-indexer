27ccd15b0a70d5a3182edc3cf486e082c85a26e3
David Uhlmann <dib542@gmail.com>
2023-06-03 17:50:13 +1000

    fix: output of TVL should be a per second timeseries

--- src/routes/timeseries/volume.ts
+++ src/routes/timeseries/volume.ts
@@ -3,7 +3,6 @@ import { Request, ResponseToolkit } from '@hapi/hapi';
 import logger from '../../logger';
 import getSwapVolumePerSecond from '../../storage/sqlite3/db/derived.tx_price_data/getSwapVolumePerSecond';
 import getTotalVolumePerSecond from '../../storage/sqlite3/db/derived.tx_price_data/getTotalVolumePerSecond';
-import hasInvertedOrder from '../../storage/sqlite3/db/dex.pairs/hasInvertedOrder';
 
 interface DataRow {
   timestamp: string;
@@ -23,16 +22,6 @@ function accumulateData(data: Data, { timestamp, tokenA, tokenB }: DataRow) {
     }
   } else {
     data.push({ timestamp, tokenA, tokenB });
-  }
-  return data;
-}
-
-function accumulateLatestDataDescending(data: Data, row: DataRow) {
-  const dataLast = data[data.length - 1];
-  // if data is ordered in descending timestamp form then the first version of
-  // a timestamp that is seen is the latest time in the resolved timestamp
-  if (!dataLast || dataLast.timestamp !== row.timestamp) {
-    data.push(row);
   }
   return data;
 }
@@ -122,72 +111,11 @@ const routes = [
     path: '/timeseries/tvl/{tokenA}/{tokenB}',
     handler: async (request: Request, h: ResponseToolkit) => {
       try {
-        const invertedOrder = await hasInvertedOrder(
-          request.params['tokenA'],
-          request.params['tokenB']
-        );
-        const volumeAtSecond = await getTotalVolumePerSecond(
+        return await getTotalVolumePerSecond(
           request.params['tokenA'],
           request.params['tokenB'],
           request.query // the time extents and frequency and such
         );
-
-        const volumeAtHour = volumeAtSecond.data.reduceRight<Data>(
-          (data, [timeUnix, amount0, amount1]) => {
-            const date = new Date(timeUnix * 1000);
-            date.setMilliseconds(0);
-            date.setSeconds(0);
-            date.setMinutes(0);
-            const row: DataRow = {
-              timestamp: date.toISOString(),
-              tokenA: invertedOrder ? amount1 : amount0,
-              tokenB: invertedOrder ? amount0 : amount1,
-            };
-            return accumulateLatestDataDescending(data, row);
-          },
-          []
-        );
-
-        const volumeAtDay = volumeAtHour.reduceRight<Data>(
-          (data, { timestamp, tokenA, tokenB }) => {
-            const date = new Date(timestamp);
-            date.setHours(0);
-            return accumulateLatestDataDescending(data, {
-              timestamp: date.toISOString(),
-              tokenA,
-              tokenB,
-            });
-          },
-          []
-        );
-
-        const volumeAtWeek = volumeAtDay.reduceRight<Data>(
-          (data, { timestamp, tokenA, tokenB }) => {
-            const date = new Date(timestamp);
-            const dayOfWeek = date.getDay();
-            // set day of month to first of a week by using dayOfWeek
-            date.setDate(date.getDate() - dayOfWeek);
-            return accumulateLatestDataDescending(data, {
-              timestamp: date.toISOString(),
-              tokenA,
-              tokenB,
-            });
-          },
-          []
-        );
-
-        return {
-          '24H': { resolution: 'hour', data: volumeAtHour.slice(0, 24) },
-          '1W': { resolution: 'hour', data: volumeAtHour.slice(0, 24 * 7) },
-          '1M': { resolution: 'day', data: volumeAtDay.slice(0, 28) },
-          '1Y': { resolution: 'week', data: volumeAtWeek.slice(0, 52) },
-          ALL:
-            volumeAtWeek.length > 52
-              ? { resolution: 'week', data: volumeAtWeek }
-              : volumeAtDay.length > 28
-              ? { resolution: 'day', data: volumeAtDay }
-              : { resolution: 'hour', data: volumeAtHour },
-        };
       } catch (err: unknown) {
         if (err instanceof Error) {
           logger.error(err);
