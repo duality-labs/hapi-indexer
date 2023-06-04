a06809dd4368950d16e18097bb382ac44279fd3d
David Uhlmann <dib542@gmail.com>
2023-06-04 20:04:34 +1000

    feat: use timeseries to generate stat point data

--- src/routes/stats/volume.ts
+++ src/routes/stats/volume.ts
@@ -1,87 +1,24 @@
 import { Request, ResponseToolkit } from '@hapi/hapi';
-import sql from 'sql-template-strings';
-import BigNumber from 'bignumber.js';
 
-import db from '../../storage/sqlite3/db/db';
+import getTotalVolume from '../../storage/sqlite3/db/derived.tx_price_data/getTotalVolume';
 import logger from '../../logger';
-
-async function volume(
-  tokenA: string,
-  tokenB: string,
-  {
-    lastDays = 1,
-    lastSeconds = lastDays * 24 * 60 * 60,
-  }: {
-    lastDays?: number;
-    lastSeconds?: number;
-  }
-) {
-  const unixNow = Math.round(Date.now() / 1000);
-  const unixStart = unixNow - lastSeconds;
-
-  return await db
-    .all(
-      sql`
-        SELECT
-          'event.PlaceLimitOrder'.'block.header.time_unix',
-          'event.PlaceLimitOrder'.'AmountIn',
-          'event.PlaceLimitOrder'.'TokenOut'
-        FROM 'event.PlaceLimitOrder'
-          WHERE
-          'event.PlaceLimitOrder'.'meta.dex.pair' = (
-            SELECT 'dex.pairs'.'id' FROM 'dex.pairs' WHERE (
-              'dex.pairs'.'token0' = ${tokenA} AND
-              'dex.pairs'.'token1' = ${tokenB}
-            ) OR (
-              'dex.pairs'.'token1' = ${tokenA} AND
-              'dex.pairs'.'token0' = ${tokenB}
-            )
-          )
-          AND 'event.PlaceLimitOrder'.'block.header.time_unix' > ${unixStart}
-      `
-    )
-    .then((rows = []) => {
-      return rows.reduce((acc, row) => {
-        // todo: add conversion to base currency to get real total value
-        return acc.plus(row['AmountIn'] ?? '0');
-      }, new BigNumber(0));
-    });
-}
 
 const routes = [
   {
     method: 'GET',
-    path: '/stats/volume/{tokenA}/{tokenB}',
+    path: '/stats/tvl/{tokenA}/{tokenB}',
     handler: async (request: Request, h: ResponseToolkit) => {
       try {
-        return {
-          hours: {
-            1: await volume(
-              request.params['tokenA'],
-              request.params['tokenB'],
-              {
-                lastSeconds: 60 * 60,
-              }
-            ),
-          },
-          days: {
-            1: await volume(
-              request.params['tokenA'],
-              request.params['tokenB'],
-              { lastDays: 1 }
-            ),
-            7: await volume(
-              request.params['tokenA'],
-              request.params['tokenB'],
-              { lastDays: 7 }
-            ),
-            28: await volume(
-              request.params['tokenA'],
-              request.params['tokenB'],
-              { lastDays: 28 }
-            ),
-          },
-        };
+        const {
+          shape,
+          data: [firstRow],
+        } = await getTotalVolume(
+          request.params['tokenA'],
+          request.params['tokenB'],
+          'second',
+          { 'pagination.limit': '1' }
+        );
+        return { shape, data: firstRow };
       } catch (err: unknown) {
         if (err instanceof Error) {
           logger.error(err);
