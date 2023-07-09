e5768fa363511210c27339368d3f43edd82dbb50
David Uhlmann <dib542@gmail.com>
2023-07-10 02:39:43 +1000

    feat: add API connection test before attempting to startup

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,32 @@ import * as sync from './sync';
 
 import routes from './routes';
 
+const { REST_API = '' } = process.env;
+
 const init = async () => {
+  // test our connection to the chain before starting
+  try {
+    logger.info(`testing connection to API: ${REST_API}`);
+
+    // fetch the transactions from block 0 (which should be empty)
+    const response = await fetch(
+      `${REST_API}/cosmos/tx/v1beta1/txs?events=tx.height=0`
+    );
+
+    if (response.status !== 200) {
+      throw new Error(`API returned status code: ${response.status}`);
+    }
+
+    const result = (await response.json()) as { txs: [] };
+    if (result && parseInt(`${result.txs.length}`) >= 0) {
+      logger.info(`connected to API: ${REST_API}`);
+    } else {
+      throw new Error(`API returned unexpected response: ${result}`);
+    }
+  } catch (err) {
+    logger.error(`connection to API failed: ${err}`);
+  }
+
   // wait for database to be set up before creating server
   await initDb();
   await initDbSchema();
