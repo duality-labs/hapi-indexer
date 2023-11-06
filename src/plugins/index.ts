17cfc9a4546c0ba3f52b3be52dd52f1c0abc3fc8
David Uhlmann <dib542@gmail.com>
2023-11-06 23:53:25 +1000

    feat: add plugin for more up-to-date IBC chain asset data

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,11 +1,14 @@
+import * as CachedChainAssetsPlugin from './cached-chain-assets';
 import * as CachedTokenPricesPlugin from './cached-token-prices';
 import * as ResponseCompressionPlugin from './response-compression';
 
 export interface GlobalPlugins
   extends ResponseCompressionPlugin.PluginContext,
+    CachedChainAssetsPlugin.PluginContext,
     CachedTokenPricesPlugin.PluginContext {}
 
 export default [
   ResponseCompressionPlugin.plugin,
+  CachedChainAssetsPlugin.plugin,
   CachedTokenPricesPlugin.plugin,
 ];
