bf1b7511ddf671d45ec787358476d4a1be86e210
David Uhlmann <dib542@gmail.com>
2023-11-10 12:56:51 +1000

    feat: add SQLite WAL mode for faster writes (#35)

--- src/storage/sqlite3/db/db.ts
+++ src/storage/sqlite3/db/db.ts
@@ -2,7 +2,7 @@ import sqlite3 from 'sqlite3';
 import { Database } from 'sqlite';
 import { Sql } from 'sql-template-tag';
 
-const { NODE_ENV, DB_FILENAME = '/tmp/database.db' } = process.env;
+const { NODE_ENV, DB_FILENAME = '/tmp/database.db', WAL = '' } = process.env;
 
 if (NODE_ENV === 'development') {
   sqlite3.verbose();
@@ -15,6 +15,10 @@ export const db = new Database({
 
 export async function init() {
   await db.open();
+  if (WAL) {
+    // enable WAL mode (significantly faster for many frequent write to file)
+    await db.exec('PRAGMA journal_mode=WAL;');
+  }
 }
 
 export default db;
