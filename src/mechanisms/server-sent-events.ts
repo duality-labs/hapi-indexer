20c706221a7880d5ac68019b508f8d1875b83ebd
David Uhlmann <dib542@gmail.com>
2023-11-15 23:07:49 +1000

    fix: make SSE consistent with long-polling for empty data sets
        - consider an empty data set to be a non-update

--- src/mechanisms/server-sent-events.ts
+++ src/mechanisms/server-sent-events.ts
@@ -102,8 +102,8 @@ export default async function serverSentEventRequest<
       // only respond able to and response is within the requested range
       if (res.writable && height <= toHeight) {
         do {
-          //  if no data is found, just send a heartbeat frame
-          if (!data) {
+          // if no data is found, just send a heartbeat frame
+          if (((data || []) as [][]).every((v) => !v?.length)) {
             res.write(
               // send event responses without data: as a "heartbeat" signal
               formatChunk({
