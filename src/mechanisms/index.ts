265449f8f1970fa77882f4c901caca05d833c3b7
David Uhlmann <dib542@gmail.com>
2023-11-22 00:54:38 +1000

    feat: always answer browser event-stream requests with event streams

--- src/mechanisms/index.ts
+++ src/mechanisms/index.ts
@@ -1,4 +1,6 @@
 import { Request, ResponseToolkit } from '@hapi/hapi';
+import { mediaTypes } from '@hapi/accept';
+
 import serverSentEventRequest from './server-sent-events';
 import longPollRequest from './long-polling';
 import { GetEndpointData, GetEndpointResponse } from './types';
@@ -18,8 +20,11 @@ export default function processRequest<
   }
 ) {
   const canUseSSE =
-    request.query['stream'] === 'true' &&
-    request.raw.req.httpVersionMajor === 2;
+    request.raw.req.httpVersionMajor === 2 &&
+    // respond to browser `new EventSource()` requests with SSE event streams
+    (!!mediaTypes(request.headers['accept']).includes('text/event-stream') ||
+      // and allow a forced stream in other cases (like manual and CI testing)
+      request.query['stream'] === 'true');
   return canUseSSE
     ? serverSentEventRequest(request, h, opts)
     : longPollRequest(request, h, opts);
