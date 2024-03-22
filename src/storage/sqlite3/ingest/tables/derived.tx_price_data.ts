d1e3597c80f1640a29f9e7ec8ea2dcc95b303398
David Uhlmann <dib542@gmail.com>
2024-03-22 19:20:10 +1000

    fix: store price events from all PlaceLimitOrders:
        - not just price changes on each index

--- src/storage/sqlite3/ingest/tables/derived.tx_price_data.ts
+++ src/storage/sqlite3/ingest/tables/derived.tx_price_data.ts
@@ -2,9 +2,8 @@ import sql from 'sql-template-tag';
 import { TxResponse } from '../../../../@types/tx';
 
 import db, { prepare } from '../../db/db';
-import selectLatestTickState from '../../db/derived.tick_state/selectLatestDerivedTickState';
 
-import { DecodedTxEvent } from '../utils/decodeEvent';
+import decodeEvent, { DecodedTxEvent } from '../utils/decodeEvent';
 import Timer from '../../../../utils/timer';
 import { selectSortedPairID } from '../../db/dex.pairs/selectPairID';
 
@@ -20,12 +19,24 @@ export default async function upsertDerivedPriceData(
     txEvent.attributes.module === 'dex' &&
     tx_result.code === 0;
 
-  if (isDexMessage && txEvent.attributes.action === 'TickUpdate') {
+  // only consider TickUpdates for price movements
+  const isDexTickUpdate =
+    isDexMessage && txEvent.attributes.action === 'TickUpdate';
+
+  // only consider TickUpdates from PlaceLimitOrder actions as price movements
+  const isDexTxMsgPlaceLimitOrder =
+    isDexTickUpdate &&
+    (tx_result.events || [])
+      .filter((txEvent) => txEvent.type === 'message')
+      .map(decodeEvent)
+      .find(
+        (txDecodedEvent) =>
+          txDecodedEvent.attributes['action'] === 'PlaceLimitOrder'
+      );
+
+  if (isDexMessage && isDexTickUpdate && isDexTxMsgPlaceLimitOrder) {
     const isForward =
       txEvent.attributes['TokenIn'] === txEvent.attributes['TokenOne'];
-    const tickSide = isForward
-      ? 'LowestNormalizedTickIndex1'
-      : 'HighestNormalizedTickIndex0';
     // note that previousTickIndex may not exist yet
     timer.start('processing:txs:derived.tx_price_data:get:tx_price_data');
     const previousPriceData = await db.get(
@@ -46,35 +57,15 @@ export default async function upsertDerivedPriceData(
       LIMIT 1
       `)
     );
-    const previousTickIndex = previousPriceData?.[tickSide];
     timer.stop('processing:txs:derived.tx_price_data:get:tx_price_data');
 
-    timer.start('processing:txs:derived.tx_price_data:get:tick_state');
     // derive data from entire ticks state (useful for maybe some other calculations)
-    const currentTickIndex: number | null = await db
-      .get(
-        ...prepare(sql`
-          WITH 'latest.derived.tick_state' AS (${selectLatestTickState(
-            txEvent.attributes['TokenZero'],
-            txEvent.attributes['TokenOne'],
-            txEvent.attributes['TokenIn'],
-            { fromHeight: 0, toHeight: Number(tx_result.height) }
-          )})
-          SELECT
-            'latest.derived.tick_state'.'TickIndex'
-          FROM
-            'latest.derived.tick_state'
-          WHERE
-            'latest.derived.tick_state'.'Reserves' != '0'
-          ORDER BY 'latest.derived.tick_state'.'TickIndex' ASC
-          LIMIT 1
-        `)
-      )
-      .then((row) => row && (isForward ? row['TickIndex'] : -row['TickIndex']));
-    timer.stop('processing:txs:derived.tx_price_data:get:tick_state');
+    const currentTickIndex: number | null = txEvent.attributes['TickIndex']
+      ? Number(txEvent.attributes['TickIndex']) * (isForward ? 1 : -1)
+      : null;
 
-    // if activity has changed current price then update data
-    if (previousTickIndex !== currentTickIndex) {
+    // if activity has a current price then update data
+    if (currentTickIndex !== null) {
       const previousOtherSideTickIndex =
         (isForward
           ? previousPriceData?.['HighestNormalizedTickIndex0']
