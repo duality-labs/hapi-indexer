5d9107ca47bffe984f5179cd9bfc9db585321ecf
David Uhlmann <dib542@gmail.com>
2024-03-29 03:17:36 +1000

    fix: calc swap volume using TokenOut to capture only executed volume:
        - and note that TokenOut volume excludes the value of fees paid.
          this is because the swap volume is a balance of the equation:
          `value in (reserves in) = value out (reserves out + fee out)`

--- src/storage/sqlite3/db/event.TickUpdate/getSwapVolume.ts
+++ src/storage/sqlite3/db/event.TickUpdate/getSwapVolume.ts
@@ -83,7 +83,7 @@ export const swapVolumeCache: PolicyOptions<DataSet> = {
               (
                 CASE
                   WHEN (
-                    'event.TickUpdate'.'TokenIn' = 'event.TickUpdate'.'TokenZero' AND
+                    'event.TickUpdate'.'TokenOut' = 'event.TickUpdate'.'TokenZero' AND
                     'event.TickUpdate'.'derived.ReservesDiff' > 0
                   )
                   THEN CAST('event.TickUpdate'.'derived.ReservesDiff' as FLOAT)
@@ -94,7 +94,7 @@ export const swapVolumeCache: PolicyOptions<DataSet> = {
               (
                 CASE
                   WHEN (
-                    'event.TickUpdate'.'TokenIn' = 'event.TickUpdate'.'TokenZero' AND
+                    'event.TickUpdate'.'TokenOut' = 'event.TickUpdate'.'TokenZero' AND
                     'event.TickUpdate'.'derived.ReservesDiff' > 0
                   )
                   THEN (
@@ -108,7 +108,7 @@ export const swapVolumeCache: PolicyOptions<DataSet> = {
               (
                 CASE
                   WHEN (
-                    'event.TickUpdate'.'TokenIn' = 'event.TickUpdate'.'TokenOne' AND
+                    'event.TickUpdate'.'TokenOut' = 'event.TickUpdate'.'TokenOne' AND
                     'event.TickUpdate'.'derived.ReservesDiff' > 0
                   )
                   THEN CAST('event.TickUpdate'.'derived.ReservesDiff' as FLOAT)
@@ -119,7 +119,7 @@ export const swapVolumeCache: PolicyOptions<DataSet> = {
               (
                 CASE
                   WHEN (
-                    'event.TickUpdate'.'TokenIn' = 'event.TickUpdate'.'TokenOne' AND
+                    'event.TickUpdate'.'TokenOut' = 'event.TickUpdate'.'TokenOne' AND
                     'event.TickUpdate'.'derived.ReservesDiff' > 0
                   )
                   THEN (
