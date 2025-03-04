ecd5d1e8aa6923332da8893d851606e42aee0e87
David Uhlmann <dib542@gmail.com>
2025-03-04 20:17:28 +1000

    refactor: quote table and column names to help syntax highlighting
        - hopefully making the SQL more readable in most code editors
        - recommend VSCode: @ext:qufiwefefwoyn.inline-sql-syntax

--- src/routes/swap-volume.ts
+++ src/routes/swap-volume.ts
@@ -45,10 +45,10 @@ export const route = {
             ? // for a "now-bound" request use the slightly-cached relevant data height
               await getCachedResponse<{ height: string }>(
                 sql`
-                  SELECT max(height) AS height
-                  FROM spacebox.dex_event_tick_update
-                  WHERE TokenZero = ${denom0}
-                    AND TokenOne = ${denom1}
+                  SELECT max("height") AS "height"
+                  FROM spacebox."dex_event_tick_update"
+                  WHERE "TokenZero" = ${denom0}
+                    AND "TokenOne" = ${denom1}
                 `,
                 {
                   cacheTime: 1 * seconds * inMs,
@@ -62,24 +62,24 @@ export const route = {
           denom: string;
         }>(
           sql`
-            WITH dex_tick_update_events_extended as (${selectDexTickUpdates})
+            WITH "dex_tick_update_events_extended" as (${selectDexTickUpdates})
             SELECT * FROM (
               SELECT
-                toStartOfInterval(timestamp, INTERVAL 1 ${raw(
+                toStartOfInterval("timestamp", INTERVAL 1 ${raw(
                   `${request.query.period}`
-                )}) AS time,
-                sumIf(SwapAmountIn, TokenIn != ${denomReporting}) +
-                sumIf(SwapAmountOut, TokenIn = ${denomReporting}) as volume,
-                ${denomReporting} as denom
-              FROM dex_tick_update_events_extended
-              WHERE timestamp >= ${unixFrom}
-                AND timestamp < ${unixTo || raw('NOW()')}
-                AND TokenZero = ${denom0}
-                AND TokenOne = ${denom1}
-              GROUP BY time
-              ORDER BY time DESC
+                )}) AS "time",
+                sumIf("SwapAmountIn", "TokenIn" != ${denomReporting}) +
+                sumIf("SwapAmountOut", "TokenIn" = ${denomReporting}) as "volume",
+                ${denomReporting} as "denom"
+              FROM "dex_tick_update_events_extended"
+              WHERE "timestamp" >= ${unixFrom}
+                AND "timestamp" < ${unixTo || raw('NOW()')}
+                AND "TokenZero" = ${denom0}
+                AND "TokenOne" = ${denom1}
+              GROUP BY "time"
+              ORDER BY "time" DESC
             )
-            WHERE volume > 0
+            WHERE "volume" > 0
             LIMIT ${LIMIT_ROWS}
           `,
           {
@@ -92,17 +92,17 @@ export const route = {
       // get 24 hour volume cached to "beginning of the hour" version
       return await getCachedResponse<{ volume: string; denom: string }>(
         sql`
-        WITH dex_tick_update_events_extended as (${selectDexTickUpdates})
+        WITH "dex_tick_update_events_extended" as (${selectDexTickUpdates})
         -- if pair contains USDC token then we can report the USDC value
         SELECT
-          sumIf(SwapAmountIn, TokenIn != ${denomReporting}) +
-          sumIf(SwapAmountOut, TokenIn = ${denomReporting}) as volume,
-          ${denomReporting} as denom
-        FROM dex_tick_update_events_extended
-        WHERE timestamp >= toStartOfHour(addDays(NOW(), -1))
-          AND timestamp < toStartOfHour(NOW())
-          AND TokenZero = ${denom0}
-          AND TokenOne = ${denom1}
+          sumIf("SwapAmountIn", "TokenIn" != ${denomReporting}) +
+          sumIf("SwapAmountOut", "TokenIn" = ${denomReporting}) as "volume",
+          ${denomReporting} as "denom"
+        FROM "dex_tick_update_events_extended"
+        WHERE "timestamp" >= toStartOfHour(addDays(NOW(), -1))
+          AND "timestamp" < toStartOfHour(NOW())
+          AND "TokenZero" = ${denom0}
+          AND "TokenOne" = ${denom1}
       `,
         {
           cacheTime: 1 * hours * inMs,
@@ -126,45 +126,45 @@ export const route = {
 const selectDexTickUpdates = sql`
   -- get indexed updates in order with an update_index field
   -- to help determine the ReservesDiff field: the current - previous Reserves value
-  WITH dex_tick_update_events_indexed AS (
+  WITH "dex_tick_update_events_indexed" AS (
     SELECT
       *,
       ROW_NUMBER() OVER (
         -- partition by "pools" of reserves (they are separate per tick + fee/tranche combination)
         -- "partition by" pool index (tick_index+fee or tick_index+tranche_key for each pair side)
-        PARTITION BY TokenZero, TokenOne, TokenIn, TickIndex, Fee, TrancheKey
+        PARTITION BY "TokenZero", "TokenOne", "TokenIn", "TickIndex", "Fee", "TrancheKey"
         -- within the pool index partition, sort by event order
-        ORDER BY height ASC, block_part_index ASC, tx_index ASC, event_index ASC
-      ) AS update_index
-    FROM spacebox.dex_event_tick_update
+        ORDER BY "height" ASC, "block_part_index" ASC, "tx_index" ASC, "event_index" ASC
+      ) AS "update_index"
+    FROM spacebox."dex_event_tick_update"
   )
   -- combine ordered updates to get relative state (ReservesDiff) and
   -- use the already derived is_swap field to compute new SwapAmountIn and SwapAmountOut attributes
   SELECT
-    current_state.*,
+    "current_state".*,
     -- get difference from last Reserves value
-    (current_state.Reserves - previous_state.Reserves) as ReservesDiff,
+    ("current_state"."Reserves" - "previous_state"."Reserves") as "ReservesDiff",
     -- note: all swap TickUpdate events should be DEX decrements (ReservesDiff < 0)
     if (
-      is_swap AND ReservesDiff < 0,
-      toUInt128(abs(ReservesDiff)),
-      current_state.SwapAmountOut
-    ) as SwapAmountOut,
+      "is_swap" AND "ReservesDiff" < 0,
+      toUInt128(abs("ReservesDiff")),
+      "current_state"."SwapAmountOut"
+    ) as "SwapAmountOut",
     -- note: SwapAmountIn may have rounding errors (but this very small in practice)
     if (
-      is_swap AND ReservesDiff < 0,
-      toUInt128(ceiling(multiply(toFloat64(abs(ReservesDiff)), pow(1.0001, TickIndex)))),
-      current_state.SwapAmountIn
-    ) as SwapAmountIn
-  FROM dex_tick_update_events_indexed as current_state
+      "is_swap" AND "ReservesDiff" < 0,
+      toUInt128(ceiling(multiply(toFloat64(abs("ReservesDiff")), pow(1.0001, "TickIndex")))),
+      "current_state"."SwapAmountIn"
+    ) as "SwapAmountIn"
+  FROM "dex_tick_update_events_indexed" as "current_state"
   -- join on same tick pool, but on the previous update
-  LEFT JOIN dex_tick_update_events_indexed as previous_state ON (
-    current_state.TokenZero = previous_state.TokenZero AND
-    current_state.TokenOne = previous_state.TokenOne AND
-    current_state.TokenIn = previous_state.TokenIn AND
-    current_state.TickIndex = previous_state.TickIndex AND
-    current_state.Fee = previous_state.Fee AND
-    current_state.TrancheKey = previous_state.TrancheKey AND
-    current_state.update_index = previous_state.update_index + 1
+  LEFT JOIN "dex_tick_update_events_indexed" as "previous_state" ON (
+    "current_state"."TokenZero" = "previous_state"."TokenZero" AND
+    "current_state"."TokenOne" = "previous_state"."TokenOne" AND
+    "current_state"."TokenIn" = "previous_state"."TokenIn" AND
+    "current_state"."TickIndex" = "previous_state"."TickIndex" AND
+    "current_state"."Fee" = "previous_state"."Fee" AND
+    "current_state"."TrancheKey" = "previous_state"."TrancheKey" AND
+    "current_state"."update_index" = "previous_state"."update_index" + 1
   )
 `;
