a5e5f843450b017b13e53fc3eab5d902fe6d7276
David Uhlmann <dib542@gmail.com>
2025-03-10 20:14:55 +1000

    feat: peat POST/query endpoint behind a ENV flag

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,9 +4,12 @@ import { route as queryRoute } from './query';
 import { route as swapVolumeRoute } from './swap-volume';
 import { route as tradesRoute } from './trades';
 
+const { NODE_ENV = '', ALLOW_POST_QUERY = '' } = process.env;
+
 const developmentRoutes = [...Object.values(debugRoutes)];
 const productionRoutes = [
-  queryRoute,
+  // optionally allow general POST/query route (unbounded query complexity)
+  ...(['1', 'true'].includes(ALLOW_POST_QUERY) ? [queryRoute] : []),
   liquidityRoute,
   swapVolumeRoute,
   tradesRoute,
@@ -14,7 +17,7 @@ const productionRoutes = [
 
 export const routes = [
   // add development only paths if needed
-  ...(process.env.NODE_ENV === 'development'
+  ...(NODE_ENV === 'development'
     ? [...developmentRoutes, ...productionRoutes]
     : productionRoutes),
 ];
