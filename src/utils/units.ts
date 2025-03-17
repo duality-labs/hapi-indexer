021987f347874ab4f0c1d2635bcdf2d73ecefa8c
David Uhlmann <dib542@gmail.com>
2025-03-17 12:57:33 +1000

    fix: use non-plurals in time period whitelist

--- src/utils/units.ts
+++ src/utils/units.ts
@@ -6,7 +6,7 @@ export const days = 24 * hours;
 // add conversion to JS milliseconds (eg. 1 * minute * inMs)
 export const inMs = 1000;
 
-export const timePeriods = ['day', 'hour', 'minute', 'seconds'] as const;
+export const timePeriods = ['day', 'hour', 'minute', 'second'] as const;
 export type TimePeriod = (typeof timePeriods)[number];
 
 // whitelist user given time period
